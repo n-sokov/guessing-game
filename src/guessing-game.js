f89c257232c66c135a621c3100e353d0ec5fb163
Nikita Sokov <nikitasokov1@gmail.com>
2024-10-23 16:31:41 +0300

    feat: complete the guessing-game task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.current = 0;
+        this.min = 0;
+        this.max = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.current = Math.round((this.min + this.max) / 2);
+        return this.current;
     }
 
     lower() {
-
+        this.max = this.current;
     }
 
     greater() {
-
+        this.min = this.current;
     }
 }
 
